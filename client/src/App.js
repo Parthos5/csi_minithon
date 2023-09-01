b3a2e22a29618f975ed3886406d24a64e9fd28fa
Hamza Sayyed <hamzawp404@gmail.com>
2023-09-01 10:12:29 +0530

    parth and hamza merge

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,23 @@ import {
   Route,
 } from "react-router-dom";
 import landingPg from "./screens/landingPg"
+import ProductDetails from "./screens/ProductDetails/ProductDetails"
+import Category from "./screens/Category/Category"
+import Cart from "./screens/Cart/Cart"
+import Wishlist from "./screens/Wishlist/Wishlist"
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Routes>
-        <Route exact path="/ll" element={<landingPg />} />
-      </Routes>
-    </div>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<landingPg />} />
+          <Route exact path="/Category" element={<Category />} />
+          <Route exact path="/ProductDetails" element={<ProductDetails />} />
+          <Route exact path="/Cart" element={<Cart />} />
+          <Route exact path="/Wishlist" element={<Wishlist />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
